afe3d8142fde2d39f61e1cd9b71bf075ccaeb3cd
Jay <j_Stfleur@apple.com>
2025-09-03 23:08:04 -0400

    * Adding functionally in mario.c to print single rows of pyramid.

---
+++ Mario.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_positive_num(void);
+void row(int n);
+
+int main(void)
+{
+
+    int get_int = get_positive_num();
+    row(get_int);
+}
+
+//gettting user input
+
+int get_positive_num(void)
+{
+  int num;
+  do
+  {
+    num = get_int("Choose a number: ");
+  }
+  while (num < 1);
+  return num;
+
+}
+
+// printing rows and col
+void row(int n)
+{
+    for (int row = 0; row < n; row++)
+    {
+      for (int col = 0; col < row; col++)
+      {
+        printf("#");
+      }
+      printf("\n");
+    }
+
+}
